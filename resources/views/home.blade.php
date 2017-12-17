fbd257452ce74b3636abd66fb6d012da65ba7840
SylvainMacabrey <sylvain.macabrey@edu.univ-fcomte.fr>
2017-12-17 11:06:24 +0100

    Amélioration affichage des posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
@@ -14,15 +14,18 @@
                         </div>
                     @endif
 
-                    You are logged in!
                     @forelse($posts as $post)
                         <div class="row">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">{{ $post->pseudo }}</div>
-                                <div class="panel-body">
+                            <div class="card">
+                                <div class="card-header">{{ $post->pseudo }}</div>
+                                <div class="card-body">
                                     <h4> {{ $post->title }} </h4>
                                     <h5> Pays visité : {{ $post->state }} </h5>
                                     <p> {{ $post->content }} </p>
+                                    <a class="btn btn-primary" {{--href="{{route('posts.show', $post)}}"--}} >Voir le post</a>
+                                </div>
+                                <div class="card-footer">
+                                    <h5>Commentaire(s)</h5>
                                 </div>
                             </div>
                         </div>
