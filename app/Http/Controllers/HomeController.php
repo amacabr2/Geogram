abe82167200d042e8bb015c02ac6338b67afc85f
SylvainMacabrey <sylvain.macabrey@edu.univ-fcomte.fr>
2017-12-20 23:07:11 +0100

    Modification méthode index() HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -15,14 +16,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $posts = DB::table('posts')
-            ->join('users', 'users.id', '=', 'posts.user_id')
+        $posts = Post::join('users', 'users.id', '=', 'posts.user_id')
             ->join('abonnements', 'abonnements.user2_id', '=', 'users.id')
             ->join('voyages', 'voyages.id', '=', 'posts.voyage_id')
             ->select('*')
             ->where('abonnements.user1_id', '=', Auth::user()->id)
             ->paginate(1);
-        //dd($posts);
         return view('home', compact('posts'));
     }
 }
